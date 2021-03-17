90f51cc18691bd69d33a3073bcf77078943fcfe2
Ernest Kim <52210251+ernestk86@users.noreply.github.com>
2021-03-16 21:53:11 -0400

    Unit tests done

---
+++ spring_app/src/test/java/com/blockbuster/AuthorizationServiceTests.java
@@ -0,0 +1,83 @@
+package com.blockbuster;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.blockbuster.exceptions.NotAuthorizedException;
+import com.blockbuster.exceptions.NotLoggedInException;
+import com.blockbuster.models.ROLE;
+import com.blockbuster.models.STATES;
+import com.blockbuster.models.User;
+import com.blockbuster.services.AuthorizationService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuthorizationServiceTests {
+	@InjectMocks
+	AuthorizationService authorizationService;
+	
+	@Mock
+	HttpServletRequest request;
+	
+	@Mock
+	HttpSession session;
+	
+	private static User currentUser;
+	private static int notCurrentUserId;
+	
+	@BeforeClass
+	public static void setUpAll() {
+		currentUser = new User(1, "First", "password", LocalDate.of(1983, 1, 13), 
+			"123 Some St.", "City", STATES.MA, 12345, Collections.emptySet(), ROLE.CUSTOMER);
+		notCurrentUserId = 0;
+	}
+	
+	@AfterClass
+	public static void tearDownAll() {
+		currentUser = null;
+	}
+	
+	@Test
+	public void testGuardByUserId() {
+		when(request.getSession(false)).thenReturn(session);
+		when(session.getAttribute("currentUser")).thenReturn(currentUser);
+		
+		authorizationService.guardByUserId(currentUser.getId());
+		
+		verify(request, times(1)).getSession(false);
+		verify(session, times(2)).getAttribute("currentUser");
+	}
+	
+	@Test(expected = NotLoggedInException.class)
+	public void testNotLoggedInException() {
+		when(request.getSession(false)).thenReturn(null);
+		authorizationService.guardByUserId(currentUser.getId());
+		
+		verify(request, times(1)).getSession(false);
+	}
+
+	@Test(expected = NotAuthorizedException.class)
+	public void testNotAuthorizedException() {
+		when(request.getSession(false)).thenReturn(session);
+		when(session.getAttribute("currentUser")).thenReturn(currentUser);
+		
+		authorizationService.guardByUserId(notCurrentUserId);
+		
+		verify(request, times(1)).getSession(false);
+		verify(session, times(2)).getAttribute("currentUser");
+	}
+}
